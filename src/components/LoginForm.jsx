79da90ab4efc3d721b942191621bee3ea0e2b1f0
Mohit Gupta <mg6489@srmist.edu.in>
2021-10-11 14:10:03 +0530

    login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -22,14 +22,14 @@ const LoginForm = () => {
       window.location.reload();
       setError('');
     } catch (err) {
-      setError('Oops, incorrect credentials.');
+      setError('Oops, invalid login,please try again.');
     }
   };
 
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Friends Hub</h1>
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
